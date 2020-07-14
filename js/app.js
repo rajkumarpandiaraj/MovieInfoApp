f5e49cc3d27739c4ec477cd3205dd1d5bea20f0c
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-07-14 05:13:43 -0700

    proj Initiated

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('#form');
+const inputValue = document.querySelector('#form-input');
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault();
+    const searchText = inputValue.value ;
+
+    getmovies(searchText);
+});
+
+function getmovies(searchText){
+    console.log(searchText);
+
+    axios.get(`http://www.omdbapi.com/?&apikey=9a76a740&s=${searchText}`)
+    .then(res => {
+        let moviesArr = res.data.Search ;
+        console.log(res);
+        // console.log(res.data.Search);
+        const movieListContainer = document.querySelector('.movielist-container');
+        let output = '' ;
+        moviesArr.forEach(movieArr => {
+            const row = `<div class = movie-list>
+                            <img src= '${movieArr.Poster}' alt = 'Poster Not Available'>
+                            <p>${movieArr.Title}</p>
+                        </div>`
+            
+            output += row ;
+        });
+
+        movieListContainer.innerHTML = output ;
+    })
+    .catch(err => console.log(err))
+}
